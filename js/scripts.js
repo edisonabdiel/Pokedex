c653f141773412e6eeec7d2a73bb34da23bc4900
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-03 09:43:25 +0200

    Add simple  object

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,24 @@ let myFavouriteFood = "Ramen";
 document.write(myFavouriteFood);
 
 alert("Hello World");
+
+let pokemonList = [
+    {
+        Name: 'Articuno',
+        Height: 1.7,
+        Weight: 55.4,
+        Type: ['legendary', 'ice']
+    },
+    {
+        Name: 'Zapdos',
+        Height: 1.6,
+        Weight: 52.6,
+        Type: ['legendary', 'shock']
+    },
+    {
+        Name: Moltres,
+        Height: 2,
+        Weight: 60,
+        Type: ['legendary', 'fire']
+    }
+];

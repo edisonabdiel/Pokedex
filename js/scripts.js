2037f1481fc21710e9d74d70483f69b82d98b2d9
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-03 19:36:30 +0200

    Add style. Implement for loop to print pokemon + its height.

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,13 @@ let pokemonList = [
         types: ['legendary', 'fire']
     }
 ];
+
+//iterates through each position of the index, checking to meet the conditions
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 2) {
+        document.write(`${pokemonList[i].name + ' height: (' + `${pokemonList[i].height}` + ')' + " Wow! That's a big pokemon!"}`)
+    } else {
+        document.write(pokemonList[i].name + ' height: (' + `${pokemonList[i].height}` + ')')
+    }
+}

61c7ed8df923766cb5badbd0ca75202eda6918e5
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-04 10:36:07 +0200

    Add <p> tags to the document parameter

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,9 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 2) {
-        document.write(`${pokemonList[i].name + ' height: (' + `${pokemonList[i].height}` + ')' + " Wow! That's a big pokemon!"}`)
+        document.write("<p>" + pokemonList[i].name + ' height: (' + pokemonList[i].height + ')' + " Wow! That's a big pokemon!" + "</p>")
     } else {
-        document.write(pokemonList[i].name + ' height: (' + `${pokemonList[i].height}` + ')')
+        document.write("<p>" + pokemonList[i].name + ' height: (' + `${pokemonList[i].height}` + ')' + "</p>")
     }
 }
+

8566cc643aa47a1b94c9c612304cc1d4d41bda0f
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-06 17:41:10 +0200

    Move the forEach() function + findPokemon into the IIEF scope. Delete old arr

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
-//initial array 
-
-let pokemonList = [
-    {
-        name: 'Articuno',
-        height: 1.7,
-        weight: 55.4,
-        types: ['legendary', 'ice']
-    },
-    {
-        name: 'Zapdos',
-        height: 1.6,
-        weight: 52.6,
-        types: ['legendary', 'shock']
-    },
-    {
-        name: 'Moltres',
-        height: 2,
-        weight: 60,
-        types: ['legendary', 'fire']
-    }
-];
-
 // refactoring the pokemonList arr into a IIFE format
-
-pokemonRep = (() => {
+let pokemonRep = (() => {
     let pokemonArr = [
         {
             name: 'Articuno',
@@ -45,26 +21,25 @@ pokemonRep = (() => {
         }
     ];
 
-    add = (pokemon) => typeof(pokemon) === typeof(pokemonArr) ? pokemonArr.push(pokemon) : "Not a valid data type";
+    add = (pokemon) => typeof (pokemon) === typeof (pokemonArr) ? pokemonArr.push(pokemon) : "Not a valid data type";
 
     getAll = () => pokemonArr;
+
+    //iterates through each position of the index, checking to meet the conditions by using a forEach()
+
+    pokemonArr.forEach((pokemon) => {
+        pokemon.height >= 2 ? document.write('<p>' + pokemon.name + " : " + pokemon.height + " That's a big pokemon!" + '</p>') : document.write('<p>' + pokemon.name + " : " + pokemon.height + '</p>')
+    });
+
+    // Filters the list by name and returns the object that gets closer to the search input 
+
+    findPokemon = () => pokemonArr.filter((pokemon) => new RegExp('Mol').test(pokemon.name))
 
     return {
         add: add,
         getAll: getAll
     };
 })();
-
-// Filters the list by name and returns the object that gets closer to the search input 
-
-findPokemon = () => pokemonList.filter((pokemon) => new RegExp('Mol').test(pokemon.name))
-
-//iterates through each position of the index, checking to meet the conditions by using a forEach()
-
-pokemonList.forEach((pokemon) => {
-   pokemon.height >= 2 ? document.write('<p>' + pokemon.name + " : " + pokemon.height + " That's a big pokemon!" + '</p>') : document.write('<p>' + pokemon.name + " : " + pokemon.height + '</p>')
-});
-
 
 console.log(add({
     name: 'Lugia',

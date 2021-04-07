f77855250511ddb5d5828fc64d5949f250343dde
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-07 16:08:03 +0200

    Add addListenItem function. Add showDetails function. Implement new method to render the pokemon array with forEach. Style elements

--- js/scripts.js
+++ js/scripts.js
@@ -21,42 +21,52 @@ let pokemonRep = (() => {
         }
     ];
 
-    function add(pokemon) { 
-        if (typeof (pokemon) === 'object') {
-          pokemonArr.push(pokemon)
-          console.log('pokemon added successfully')
-        } else {
-          console.log("Not a valid data type")
-        }
-      }
+    //adds a new pokemon to the array
 
-    function getAll(){ return pokemonArr};
+    function add(pokemon) { (typeof (pokemon) === 'object') ? pokemonArr.push(pokemon) : console.log("Not a valid data type") };
+
+    //returns the whole array
+
+    function getAll() { return pokemonArr };
 
     // Filters the list by name and returns the object that gets closer to the search input 
 
     function findPokemon() { pokemonArr.filter((pokemon) => new RegExp('Lu').test(pokemon.name)) }
 
+    //creates a button for each pokemon and shows it's details on click
+
+    function addListItem(pokemon) {
+        let pokeList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listItem.appendChild(button);
+        pokeList.appendChild(listItem);
+        button.addEventListener('click', showDetails(pokemon));
+    };
+
+    let showDetails = (pokemon) => console.log(pokemon);
+
     return {
         add: add,
         getAll: getAll,
-        findPokemon: findPokemon
+        findPokemon: findPokemon,
+        addListItem: addListItem,
     };
 })();
 
-//iterates through each position of the index, checking to meet the conditions by using a forEach()
+//iterates through each position of the index
 
-pokemonRep.getAll().forEach((pokemon) => {
-    pokemon.height >= 2 ? document.write('<p>' + pokemon.name + " : " + pokemon.height + " That's a big pokemon!" + '</p>') : document.write('<p>' + pokemon.name + " : " + pokemon.height + '</p>')
-});
+pokemonRep.getAll().forEach((pokemon) => pokemonRep.addListItem(pokemon));
 
-
+//adds a new pokemon to the array
 
 pokemonRep.add({
     name: 'MeowTwo',
     height: 2,
     weight: 60,
     types: ['legendary', 'fire']
-})
+});
 
-
-console.log(pokemonRep.getAll())
+console.log(pokemonRep.getAll());

1874271da0bc2dce65c236b099cb251fd3fc675d
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-04-20 12:18:17 +0200

    Minify both css and JS files. Add .eslintrc.js. Add dist folder and its respective minified files for both css and JS

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      jquery: true,
+    },
+    extends: [
+      'airbnb-base',
+      'eslint:recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 12,
+    },
+    rules: {
+      quotes: ['error', 'single'],
+    },
+};
+  
